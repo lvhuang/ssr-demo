b2461553a3c4c1d55ad38e477e08b9f2b93227eb
qiangyongjun <qiangyj@feidai.com>
2020-05-13 10:39:24 +0800

    bug 修复

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!--  <img alt="Vue logo" src="./assets/logo.png"> -->
     <nav>
       <router-link to="/">首页</router-link>
       <router-link to="/detail">详情页</router-link>
@@ -11,13 +10,9 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
